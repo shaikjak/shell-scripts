e3d1f9deaa3b180555faeb6dd5b0952d710978e8
shaikjak <dudekulajakeer143@gmail.com>
2025-10-21 18:41:19 +0530

    updated arg.sh

---
+++ argument.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$1:: $2 how are you , how is life is going"
+echo "$2:: $1 i am good and how are you?"
+echo "$1:: $2 where are you working currently"
+echo "$2:: $1 " i am working in hyd"
